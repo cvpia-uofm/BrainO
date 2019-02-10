2f30ce6bac48a142b827b17a8cd807bbec2185b9
Kazi Moinuddin <kmnuddin@memphis.edu>
2019-02-10 13:57:56 -0600

    Initial Commit v0.1

---
+++ Assets/Func_Area_Model/Regions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Func_Area_Model
+{
+    public class Regions
+    {
+        public string Abbreviation { get; set; }
+        public string Region_Name { get; set; }
+        public Vector3 Region_Co_Ord { get; set; }
+        public enum Hemisphere { Right, Left }
+        public string Note;
+    }
+}
---
+++ Assets/Scripts/BrainController.cs
@@ -0,0 +1,54 @@
+﻿using Assets.Func_Area_Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class BrainController : MonoBehaviour
+{
+    public Transform Func_Areas_Pos;
+    public GameObject Func_Area;
+    const float rotateSpeed = 200;
+    IEnumerable<Regions> regions;
+
+    private void Awake()
+    {
+        regions = new List<Regions>();
+        LoadFunc_Area_Pos();
+    }
+
+    private void LoadFunc_Area_Pos()
+    {
+       
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            float rotateX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
+            float rotateY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
+
+            transform.Rotate(Vector3.up, -rotateX);
+            transform.Rotate(Vector3.right, -rotateY); 
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            gameObject.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+            //gameObject.transform.Translate(Input.mousePosition.x, Input.mousePosition.y, Input.GetAxis("Mouse ScrollWheel") * 30f); 
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            gameObject.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+            //gameObject.transform.Translate(Input.mousePosition.x, Input.mousePosition.y, Input.GetAxis("Mouse ScrollWheel") * 30f); 
+        }
+    }
+}
