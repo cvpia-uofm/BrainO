662a082bcbec316d8b36d5bb384f6519f9287b0a
Kazi Moinuddin <kmnuddin@memphis.edu>
2019-02-11 19:43:58 -0600

    Configuring Functional Areas

--- Assets/Scripts/BrainController.cs
+++ Assets/Scripts/BrainController.cs
@@ -10,51 +10,42 @@ using AutoMapperFactory;
 
 public class BrainController : MonoBehaviour
 {
-    public Transform Func_Areas_Pos;
-    public GameObject Func_Area;
     const float rotateSpeed = 200;
-    IEnumerable<Regions> regions;
-
-    private void Awake()
-    {
-        regions = new List<Regions>();
-        LoadFunc_Area_Pos();
-    }
-
-    private void LoadFunc_Area_Pos()
-    {
-        //regions = ExcelFactory<Regions>.Map("..//Assets//Regions//desikan_atlas.csv");
-        TextAsset data_raw = Resources.Load<TextAsset>("Data");
-        string[] data = data_raw.text.Split(new char[] { '\n' });
-        regions = MapperFactory<Regions>.Map_CSV(data);
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
     private void Update()
     {
+        Rotation();
+        Scaling();
+    }
 
+    private void Scaling()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (!gameObject.transform.localScale.Equals(new Vector3(100, 100, 100)))
+            {
+                gameObject.transform.localScale += new Vector3(1f, 1f, 1f);
+            }
+            //gameObject.transform.Translate(Input.mousePosition.x, Input.mousePosition.y, Input.GetAxis("Mouse ScrollWheel") * 30f); 
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (!gameObject.transform.localScale.Equals(new Vector3(40, 40, 40)))
+            {
+                gameObject.transform.localScale -= new Vector3(1f, 1f, 1f); 
+            }
+            //gameObject.transform.Translate(Input.mousePosition.x, Input.mousePosition.y, Input.GetAxis("Mouse ScrollWheel") * 30f); 
+        }
+    }
+
+    private void Rotation()
+    {
         if (Input.GetMouseButton(0))
         {
             float rotateX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
             float rotateY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
 
             transform.Rotate(Vector3.up, -rotateX);
-            transform.Rotate(Vector3.right, -rotateY); 
-        }
-
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
-        {
-            gameObject.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
-            //gameObject.transform.Translate(Input.mousePosition.x, Input.mousePosition.y, Input.GetAxis("Mouse ScrollWheel") * 30f); 
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
-            gameObject.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
-            //gameObject.transform.Translate(Input.mousePosition.x, Input.mousePosition.y, Input.GetAxis("Mouse ScrollWheel") * 30f); 
+            transform.Rotate(Vector3.right, -rotateY);
         }
     }
 }
---
+++ Assets/Scripts/PointsController.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Func_Area_Model;
+using AutoMapperFactory;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsController : MonoBehaviour
+{
+    public GameObject Func_Area;
+    IEnumerable<Regions> regions;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        regions = new List<Regions>();
+        LoadFunc_Area_Pos();
+    }
+
+    private void LoadFunc_Area_Pos()
+    {
+        //regions = ExcelFactory<Regions>.Map("..//Assets//Regions//desikan_atlas.csv");
+        TextAsset data_raw = Resources.Load<TextAsset>("Data");
+        string[] data = data_raw.text.Split(new char[] { '\n' });
+        regions = MapperFactory<Regions>.Map_CSV(data);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GenerateCo_Ord();
+    }
+
+    private void GenerateCo_Ord()
+    {
+        foreach (var region in regions)
+        {
+            float X = (float)(region.X);
+            float Y = (float)(region.Y);
+            float Z = (float)(region.Z);
+            var pos = new Vector3(X, Y, Z);
+            Func_Area.transform.position = pos;
+            Instantiate(Func_Area, Func_Area.transform);
+            Func_Area.transform.SetParent(gameObject.Find("Points").transform);
+        }
+    }
+}
